3d684bfed3b5f59ab9b19c06d1a03203effbe0fe
Vasilev <vasvova95@mail.ru>
2016-02-15 11:25:07 +0300

    1 задание

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Всем привет!" );
+            Console.Write("Меня зовут Андрей! ");
+            Console.Write("Я люблю Ставрополь! " );
+            Console.Write("Интерисуюсь баскетболом! ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 2;
+            Console.WriteLine("     ********     ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 3;
+            Console.WriteLine("    **********     ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 4;
+            Console.WriteLine("   ************    ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 5;
+            Console.WriteLine("  ***  ***  ***   ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 6;
+            Console.WriteLine("  **************   ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 7;
+            Console.WriteLine("  **  *****   **  ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 8;
+            Console.WriteLine("   *          *   ");
+            Console.CursorLeft = 0;
+            Console.CursorTop = 9;
+            Console.WriteLine("    **********    ");
+            Console.ReadKey();
+            Console.CursorLeft = 5;
+            Console.CursorTop = 9;
+            Console.WriteLine("    *         ");
+            Console.CursorLeft = 9;
+            Console.CursorTop = 9;
+            Console.WriteLine("        *     ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication7/ConsoleApplication7/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1. Приложение для расчета ЗП.
+
+            int i;
+            int d = 0;
+            int z;
+            int a=0;
+            int c=0;
+
+            Console.Write("Введите колличество дней, которое вы работали в неделю: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            if (d <= 7)
+            {
+                if (d > 5)
+                {
+                    Console.WriteLine("Заработная плата в выходные дни начисляется по двойному тарифу.");
+                }
+
+                do
+                {
+                    Console.WriteLine("Введите размер дневной выручки: ");
+                    i = Convert.ToInt32(Console.ReadLine());
+
+                    if (i <= 10000)
+                    {
+                        z = i * 5 / 100;
+                        Console.WriteLine("Размер заработной платы равен: " + z);
+                    }
+
+                    else
+                    {
+                        z = (i-10000) * 10 / 100 + 500;
+                        Console.WriteLine("Размер заработной платы равен: " + z);
+                    }
+
+                    if (z < 300)
+                    {
+                        z = 300;
+                        Console.WriteLine("Минимальный размер оплаты труда за 1 день равен 300р. ");
+                    }
+
+                    a++;
+
+
+                    if (a > 5)
+                    {
+                        z = z * 2;
+                        Console.WriteLine("Внимание! Двойной тариф. Заработная плата за этот день равна: " + z);
+                    }
+
+                    c = c + z;
+
+                    if (a == d)
+                    {
+                        Console.WriteLine("Заработная плата составляет: " + c);
+                    }
+                }
+                while (a < d);
+            }
+                else
+            {
+                Console.WriteLine("Кол во дней превышено ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
